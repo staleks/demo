6727883e8caff7e82f656f6ea876c67a1377d40a
astoisavljevic <astoisavljevic@jatheon.com>
2019-02-23 09:07:39 +0100

    initial commit

---
+++ src/main/java/com/example/demo/endpoint/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.endpoint;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> handle() {
+        return ResponseEntity.ok("Ok");
+    }
+
+}
