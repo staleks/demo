6d9b340d704869472f0afd93831a1150613f6a93
astoisavljevic <astoisavljevic@jatheon.com>
2019-02-23 09:13:09 +0100

    adding logger

--- src/main/java/com/example/demo/endpoint/TestController.java
+++ src/main/java/com/example/demo/endpoint/TestController.java
@@ -1,14 +1,18 @@
 package com.example.demo.endpoint;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/")
     public ResponseEntity<String> handle() {
+        LOG.info("test");
         return ResponseEntity.ok("Ok");
     }
 
