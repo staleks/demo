3997cd4ac97a0b34c7a2d0fe5a581e36719cf712
astoisavljevic <astoisavljevic@jatheon.com>
2019-02-25 16:30:13 +0100

    working on Docker Flow

--- src/main/java/com/example/demo/endpoint/TestController.java
+++ src/main/java/com/example/demo/endpoint/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
 
     @GetMapping("/")
     public ResponseEntity<String> handle() {
-        LOG.info("test");
+        LOG.info("test #2");
         return ResponseEntity.ok("Ok");
     }
 
