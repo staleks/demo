a121cb6cd72059938138313b29cb5f89cb8de2d0
astoisavljevic <astoisavljevic@jatheon.com>
2019-02-25 16:57:11 +0100

    tryut

--- src/main/java/com/example/demo/endpoint/TestController.java
+++ src/main/java/com/example/demo/endpoint/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
 
     @GetMapping("/")
     public ResponseEntity<String> handle() {
-        LOG.info("test #3");
+        LOG.info("test #4");
         return ResponseEntity.ok("Ok");
     }
 
